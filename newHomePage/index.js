9e399f47d92c9c899f64929c01ac309b9742b81a
cm-ryffel <dimanlancev0@gmail.com>
2023-10-09 21:32:04 +0300

    secondary

--- newHomePage/index.js
+++ newHomePage/index.js
@@ -1,9 +1,43 @@
+// try {
+//   const navItems = document.querySelectorAll('.nav-item');
+//   const menuContents = document.querySelectorAll('.menu-content');
+//   const burgerNavButton = document.querySelector('.navbar-toggler');
+
+//   menuContents.forEach(content => {
+//     if (content.id !== 'tasks') {
+//       content.style.display = 'none';
+//     }
+//   });
+
+//   navItems.forEach((item, index) => {
+//     item.addEventListener('click', () => {
+//       menuContents.forEach(content => content.style.display = 'none');
+//       menuContents[index].style.display = 'block';
+//     });
+//   });
+
+//   burgerNavButton.addEventListener('click', () => {
+//     const targetId = burgerNavButton.getAttribute('data-bs-target');
+//     const targetMenu = document.querySelector(targetId);
+//     const targetNavItems = targetMenu.querySelectorAll('.nav-elem');
+
+//     targetNavItems.forEach((item, index) => {
+//       item.addEventListener('click', () => {
+//         menuContents.forEach(content => content.style.display = 'none');
+//         menuContents[index].style.display = 'block';
+//         burgerNavButton.click();
+//       });
+//     });
+//   });
+// } catch (error) {
+//   console.error(`Ошибка: ${error.message}`);
+// }
+
 try {
-  const navItems = document.querySelectorAll('.nav-elem');
+  const navItems = document.querySelectorAll('.nav-item');
   const menuContents = document.querySelectorAll('.menu-content');
   const burgerNavButton = document.querySelector('.navbar-toggler');
 
-  // Скрываем все элементы, кроме #tasks
   menuContents.forEach(content => {
     if (content.id !== 'tasks') {
       content.style.display = 'none';
@@ -11,10 +45,12 @@ try {
   });
 
   navItems.forEach((item, index) => {
-    item.addEventListener('click', () => {
-      menuContents.forEach(content => content.style.display = 'none');
-      menuContents[index].style.display = 'block';
-    });
+    if (!item.classList.contains('user-pic')) {
+      item.addEventListener('click', () => {
+        menuContents.forEach(content => content.style.display = 'none');
+        menuContents[index].style.display = 'block';
+      });
+    }
   });
 
   burgerNavButton.addEventListener('click', () => {
@@ -23,11 +59,13 @@ try {
     const targetNavItems = targetMenu.querySelectorAll('.nav-elem');
 
     targetNavItems.forEach((item, index) => {
-      item.addEventListener('click', () => {
-        menuContents.forEach(content => content.style.display = 'none');
-        menuContents[index].style.display = 'block';
-        burgerNavButton.click();
-      });
+      if (!item.classList.contains('user-pic')) {
+        item.addEventListener('click', () => {
+          menuContents.forEach(content => content.style.display = 'none');
+          menuContents[index].style.display = 'block';
+          burgerNavButton.click();
+        });
+      }
     });
   });
 } catch (error) {
